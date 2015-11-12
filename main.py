3953ad3dd298e7d2ab59e895ad19d4ad1877bcc1
azevedus <leoazeveds@gmail.com>
2015-11-12 04:37:52 -0200

    Build test update
    Added letter based sorting and number based selection

--- main.py
+++ main.py
@@ -7,16 +7,47 @@ def soupCrawler(soupLink):
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
 
+def commandLetter():
+
+    print ("Please input the starter letter of the anime you want to download: ")
+    global selectedLetter
+    selectedLetter = raw_input()
+    print ("")
+
 def mangaList_spider():
 
-    soupCrawler('http://www.crunchyroll.com/videos/anime/alpha?group=all')
-    for mangaListInfo in soup.findAll(token="shows-portraits"):
-        title = mangaListInfo.string.encode('utf-8')
-        link = 'http://www.crunchyroll.com' + mangaListInfo.get('href')
+    soupCrawler('http://www.crunchyroll.com/videos/anime/alpha?group='+selectedLetter)
+    for leftGrid in soup.findAll('div', {'id':'main_content'}):
+        i = 1
+        selectedLetterList = leftGrid.findAll('span', {'class':'series-title block ellipsis'})
+        for mangaListInfo in selectedLetterList:
 
-        print title
-        print link
+            title = mangaListInfo.string.encode('utf-8')
+            print str(i) + " - " + title
+            i += 1
 
+def selectedSeries_spider():
+    print ("")
+    print ("Please enter the number correspondent to the series you want to download "
+           "or 'exit' to return to the previous menu")
+    selectedSeries = raw_input()
+    selectedSeries = str(selectedSeries)
 
+    if selectedSeries == "exit":
+        print ("")
+        finalRun()
+    else:
+        selectedSeries = int(selectedSeries)
+        selectedSeries -= 1
 
-mangaList_spider()
+        for leftGrid in soup.findAll('div', {'id':'main_content'}):
+            selectedLetterList = leftGrid.findAll('span', {'class':'series-title block ellipsis'})[selectedSeries].parent.parent
+            selectedAnimeUrl = 'http://www.crunchyroll.com' + selectedLetterList.get('href')
+            print selectedAnimeUrl
+
+def finalRun():
+    commandLetter()
+    mangaList_spider()
+    selectedSeries_spider()
+
+finalRun()
